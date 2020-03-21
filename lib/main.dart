b518eb02068bca3e10a3a8774852aff10b0ef0c3
Macen Vargas <mvargas9517@live.com>
2020-03-20 19:11:56 -0500

    Added middle boxes

--- lib/main.dart
+++ lib/main.dart
@@ -30,20 +30,21 @@ class HomePage extends StatelessWidget {
         decoration: BoxDecoration(
           gradient: LinearGradient(colors: [Colors.pink[400].withOpacity(0.9), Colors.orange[800].withOpacity(0.8)])
         ),),
-        Padding(
-          padding: const EdgeInsets.only(top: 30.0, left: 25, right: 25),
-          child: Column(
+            Column(
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Entypo.arrow_long_left,
-                  size: 35,
-                  color: Colors.white),
-                  Icon(EvilIcons.gear,
-                  size: 40,
-                  color: Colors.white),
-                ],
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Entypo.arrow_long_left,
+                    size: 35,
+                    color: Colors.white),
+                    Icon(EvilIcons.gear,
+                    size: 40,
+                    color: Colors.white),
+                  ],
+                ),
               ),
               SizedBox(height: _height * 0.01),
               Column(
@@ -55,34 +56,52 @@ class HomePage extends StatelessWidget {
                     letterSpacing: 0.8,
                     color: Colors.white
                   )),
+                  SizedBox(height: _height * 0.01,),
                   Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       Text('Flutter Developer',
                       style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 14,
                         letterSpacing: 0.8,
                         color: Colors.white
                       )),
-                      Container(
-                        color: Colors.white,
-                        width: 10,
-                        height: 10,
-                        child: VerticalDivider(color: Colors.white,),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: Container( color: Colors.white, height: 10, width: 1, ),
                       ),
                       Text('Front End Developement',
                       style: TextStyle(
-                        fontSize: 15,
+                        fontSize: 14,
                         letterSpacing: 0.8,
                         color: Colors.white
                       )),
                     ],
                   ),
+                  SizedBox(height: _height * 0.01,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(MaterialIcons.location_on,
+                      size: 20,
+                      color: Colors.white,),
+                      Text('Wichita,USA',
+                        style: TextStyle(
+                          letterSpacing: 0.5,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500
+                        ),),
+                    ],
+                  ),
                 ],
-              )
+              ),
+              SizedBox(height: _height * 0.15),
+              Container(
+                height: _height * 0.6078,
+                color: Colors.white,
+              ),
             ],
           ),
-        ),
-          ],
       );
   }
 }
