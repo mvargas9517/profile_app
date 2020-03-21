69c83b6a488d3ecc992caf74959a85c30e6e739a
Macen Vargas <mvargas9517@live.com>
2020-03-21 12:33:07 -0500

    Finished first page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'dart:math' as math;
 
 void main() => runApp(MyApp());
 
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: HomePage(),
       ),
@@ -26,6 +28,12 @@ class HomePage extends StatelessWidget {
 
     return Stack(
           children: <Widget>[
+            Positioned(
+                top: 0,
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationY(math.pi),
+                    child: Image(image: AssetImage('images/macen.jpeg'), height: _height * 0.4, width: _width* 0.6,))),
             Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(colors: [Colors.pink[400].withOpacity(0.9), Colors.orange[800].withOpacity(0.8)])
@@ -70,7 +78,7 @@ class HomePage extends StatelessWidget {
                         padding: const EdgeInsets.symmetric(horizontal: 5),
                         child: Container( color: Colors.white, height: 10, width: 1, ),
                       ),
-                      Text('Front End Developement',
+                      Text('Front End Development',
                       style: TextStyle(
                         fontSize: 14,
                         letterSpacing: 0.8,
@@ -78,7 +86,7 @@ class HomePage extends StatelessWidget {
                       )),
                     ],
                   ),
-                  SizedBox(height: _height * 0.01,),
+                  SizedBox(height: _height * 0.01),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
@@ -96,12 +104,184 @@ class HomePage extends StatelessWidget {
                 ],
               ),
               SizedBox(height: _height * 0.15),
-              Container(
-                height: _height * 0.6078,
-                color: Colors.white,
+              Stack(
+                children: <Widget>[
+                  Container(
+                    color: Colors.white,
+                    height: _height * 0.6078,
+                  ),
+                  Positioned(
+                    top: -65,
+                    right: 38,
+                    child: Column(
+                      children: <Widget> [
+                        Row(
+                        children: <Widget>[
+                          RawMaterialButton(
+                            onPressed: () {},
+                            child: new Icon(
+                              AntDesign.dribbble,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            shape: new CircleBorder(),
+                            elevation: 2.0,
+                            fillColor: Colors.pink,
+                            padding: const EdgeInsets.all(10),
+                          ),
+                          SizedBox(width: _width * 0.02),
+                          Material(
+                            elevation: 4.0,
+                            shape: CircleBorder(),
+                            clipBehavior: Clip.hardEdge,
+                            color: Colors.transparent,
+                            child: Ink.image(
+                              image: AssetImage('images/macen.jpeg'),
+                              fit: BoxFit.cover,
+                              width: 130,
+                              height: 130,
+                              child: InkWell(
+                                onTap: () {},
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: _width * 0.02),
+                          RawMaterialButton(
+                            onPressed: () {},
+                            child: new Icon(
+                              AntDesign.behance,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                            shape: new CircleBorder(),
+                            elevation: 2.0,
+                            fillColor: Colors.pink,
+                            padding: const EdgeInsets.all(10),
+                          ),
+                        ],
+                      ),
+                        SizedBox(height: _height * 0.055),
+                        Row(
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text('1755',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.pink,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                SizedBox(height: 3,),
+                                Text('Apprecitions',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey
+                                  ),),
+                              ],
+                            ),
+                            SizedBox(width: _width * 0.07),
+                            Column(
+                              children: <Widget>[
+                                Text('800',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: Colors.pink,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                SizedBox(height: 3,),
+                                Text('Followers',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.grey
+                                  ),),
+                              ],
+                            ),
+                            SizedBox(width: _width * 0.07),
+                            Column(
+                              children: <Widget>[
+                                Text('231',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.pink,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                                SizedBox(height: 3,),
+                                Text('Following',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.grey
+                                ),),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: _height* 0.07),
+                     ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 110,
+                    left: 30,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('About Macen',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 0.5,
+                        )),
+                        SizedBox(height: 13),
+                        Container(
+                          width: _width * 0.535,
+                            child: Text('I\'m a self taught developer and have been mainly using flutter for about 8 months. I like to create'
+                                'user interfaces and designs that peak my interest. Would love to try and make your design so feel free to reach out!',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),),
+                        ),],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 28,
+                    left: 110,
+                    child: Container(
+                      height: 45.0,
+                      child: RaisedButton(
+                        onPressed: () {},
+                        elevation: 8.0,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+                        padding: EdgeInsets.all(0.0),
+                        child: Ink(
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [Colors.pink[400].withOpacity(0.9), Colors.orange[800].withOpacity(0.8)],
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0)
+                          ),
+                          child: Container(
+                            constraints: BoxConstraints(maxWidth: 200.0, minHeight: 50.0),
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Follow",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+                overflow: Overflow.visible,
               ),
             ],
           ),
+         ],
       );
   }
 }
---
+++ lib/profile.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue);
+  }
+}
